0654e2c86eac3faa2777b746bae47c9a01b9fd54
Austin <ndkhiem3010@gmail.com>
2023-05-17 17:03:23 +0800

    Adding FooterView to NavigateCard component

--- components/NavigateCard.tsx
+++ components/NavigateCard.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import tw from 'twrnc';
@@ -9,6 +9,7 @@ import { setDestination } from '../slices/navSlice';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '../types/navigation';
 import NavFavourties from './NavFavourties';
+import { Icon } from '@rneui/themed/dist/Icon';
 
 const NavigateCard = () => {
 	const dispatch = useDispatch();
@@ -44,6 +45,35 @@ const NavigateCard = () => {
 				</View>
 
 				<NavFavourties />
+			</View>
+			<View
+				style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}
+			>
+				<TouchableOpacity
+					style={tw`flex flex-row bg-black justify-between w-24 px-4 py-3 rounded-full`}
+					onPress={() => {
+						navigation.navigate('RideOptionsCard');
+					}}
+				>
+					<Icon
+						name='car'
+						type='font-awesome'
+						color='white'
+						size={16}
+					></Icon>
+					<Text style={tw`text-white text-center`}>Rides</Text>
+				</TouchableOpacity>
+				<TouchableOpacity
+					style={tw`flex flex-row justify-between w-24 px-4 py-3 rounded-full`}
+				>
+					<Icon
+						name='fast-food-outline'
+						type='ionicon'
+						color='black'
+						size={16}
+					></Icon>
+					<Text style={tw`text-black text-center`}>Eats</Text>
+				</TouchableOpacity>
 			</View>
 		</SafeAreaView>
 	);
